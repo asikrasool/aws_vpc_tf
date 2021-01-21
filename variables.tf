5b3771dbdd270188b22f700185fc9120089ac652
rasoolasik <arasool@ecloudcontrol.com>
2021-01-21 07:13:05 +0000

    Added VPC with Public and Private Subnet

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+     default = "AKIAJ2Q4X7HONHYV3WQQ"
+}
+variable "secret_key" {
+     default = "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"
+}
+variable "region" {
+     default = "ap-south-1"
+}
